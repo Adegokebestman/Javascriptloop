6f3ed53f1a8268362ee509cc5a7599f16fa9f597
thecoderboy <adegokebestman@gmail.com>
2020-03-25 10:35:14 +0100

    script

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = "Adegoke Bestman";
+var courses = ["HTML", "css", "Javascript", "design"];
+alert(name);
+alert(courses);
+for(courses = 1; courses < 200; courses +=2 ){
+	console.log(courses)
+}
